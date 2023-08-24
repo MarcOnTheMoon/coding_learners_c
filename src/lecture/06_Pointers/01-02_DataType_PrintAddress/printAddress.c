0db56265eae117f4bb6f0939d68035c503811151
Marc Hensel <121319337+MarcOnTheMoon@users.noreply.github.com>
2023-08-24 14:09:41 +0200

    Add files via upload

--- src/lecture/06_Pointers/01-02_DataType_PrintAddress/printAddress.c
+++ src/lecture/06_Pointers/01-02_DataType_PrintAddress/printAddress.c
@@ -3,7 +3,7 @@
  * Variables with same name in different functions have different addresses (i.e., are at not the same).
  *****************************************************************************************************
  * Author: Marc Hensel, http://www.haw-hamburg.de/marc-hensel
- * Project: https://github.com/MarcOnTheMoon/coding_learners/
+ * Project: https://github.com/MarcOnTheMoon/coding_learners_c/
  * Copyright: 2023, Marc Hensel
  * Version: 2023.08.23
  * License: CC BY-NC-SA 4.0, see https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
--- src/lecture/06_Pointers/03-06_Arrays_MinMax/trickyMinMax.c
+++ src/lecture/06_Pointers/03-06_Arrays_MinMax/trickyMinMax.c
@@ -3,7 +3,7 @@
  * Determine min and max value in an array (tricky approach).
  *****************************************************************************************************
  * Author: Marc Hensel, http://www.haw-hamburg.de/marc-hensel
- * Project: https://github.com/MarcOnTheMoon/coding_learners/
+ * Project: https://github.com/MarcOnTheMoon/coding_learners_c/
  * Copyright: 2023, Marc Hensel
  * Version: 2023.08.23
  * License: CC BY-NC-SA 4.0, see https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
