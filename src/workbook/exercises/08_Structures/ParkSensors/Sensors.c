49e33c8bfe91695c01a85fc73f74f3517dba3164
Marc Hensel <121319337+MarcOnTheMoon@users.noreply.github.com>
2023-08-24 14:25:45 +0200

    Add files via upload

--- src/workbook/exercises/08_Structures/ParkSensors/Sensors.c
+++ src/workbook/exercises/08_Structures/ParkSensors/Sensors.c
@@ -2,7 +2,7 @@
  * Sample solution
  *****************************************************************************************************
  * Author: Marc Hensel, http://www.haw-hamburg.de/marc-hensel
- * Project: https://github.com/MarcOnTheMoon/coding_learners/
+ * Project: https://github.com/MarcOnTheMoon/coding_learners_c/
  * Copyright: 2023, Marc Hensel
  * Version: 2023.08.10
  * License: CC BY-NC-SA 4.0, see https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
