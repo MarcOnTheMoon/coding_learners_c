807827b62c1af29f1f8fcb5e8194485b7cbc6925
Marc Hensel <121319337+MarcOnTheMoon@users.noreply.github.com>
2023-08-24 14:07:24 +0200

    Add files via upload

--- src/lecture/09_Lists/02-01_Sort_Integers/sortInteger.c
+++ src/lecture/09_Lists/02-01_Sort_Integers/sortInteger.c
@@ -3,7 +3,7 @@
  * Bubble sort, single-pass sorting approach, and quicksort for integer arrays.
  *****************************************************************************************************
  * Author: Marc Hensel, http://www.haw-hamburg.de/marc-hensel
- * Project: https://github.com/MarcOnTheMoon/coding_learners/
+ * Project: https://github.com/MarcOnTheMoon/coding_learners_c/
  * Copyright: 2023, Marc Hensel
  * Version: 2023.08.23
  * License: CC BY-NC-SA 4.0, see https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
