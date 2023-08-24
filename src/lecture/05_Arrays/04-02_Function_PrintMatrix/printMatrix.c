0fc2050adb4b1414c19398e3e1963aa89dacc431
Marc Hensel <121319337+MarcOnTheMoon@users.noreply.github.com>
2023-08-24 14:12:06 +0200

    Add files via upload

--- src/lecture/05_Arrays/04-02_Function_PrintMatrix/printMatrix.c
+++ src/lecture/05_Arrays/04-02_Function_PrintMatrix/printMatrix.c
@@ -3,7 +3,7 @@
  * 2-D array as function parameter.
  *****************************************************************************************************
  * Author: Marc Hensel, http://www.haw-hamburg.de/marc-hensel
- * Project: https://github.com/MarcOnTheMoon/coding_learners/
+ * Project: https://github.com/MarcOnTheMoon/coding_learners_c/
  * Copyright: 2023, Marc Hensel
  * Version: 2023.08.23
  * License: CC BY-NC-SA 4.0, see https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
